f401f24b5a83634c0e12ee5bba1911de90c5cab5
zhangdongkun <zhangdongkun@sinosoft.com.cn>
2019-08-14 11:44:34 +0800

    first commit

---
+++ src/main/java/cn/zdk/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,27 @@
+package cn.zdk;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @author zdk
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    public MyBeanFactoryPostProcessor() {
+        super();
+        System.out.println("这是BeanFactoryPostProcessor实现类构造器！！");
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
+            throws BeansException {
+        System.out
+                .println("BeanFactoryPostProcessor调用postProcessBeanFactory方法");
+        BeanDefinition bd = arg0.getBeanDefinition("person");
+        bd.getPropertyValues().addPropertyValue("phone", "110");
+    }
+
+}
